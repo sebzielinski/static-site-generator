1e046c9986c8a7fcd006d6e62bf99ecd925c977c
Sebastian Zielinski <szielinski@uni-osnabrueck.de>
2024-10-10 13:54:21 +0200

    update

--- src/conversion.py
+++ src/conversion.py
@@ -1,6 +1,6 @@
 from textnode import TextNode
 from leafnode import LeafNode
-
+import re
 
 text_types = ["text", "bold", "italic", "code", "link", "image"] 
 
@@ -21,3 +21,99 @@ def text_node_to_html_node(text_node):
             return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
     
     raise ValueError
+
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if len(old_nodes) == 0:
+        raise ValueError("The node list should not be empty.")
+
+    new_nodes = []
+    for node in old_nodes:
+        split_node = node.text.split(delimiter)
+        if len(split_node) == 1:
+            new_nodes.append(node)
+            continue
+        elif len(split_node) % 2 == 0:
+            raise SyntaxError("Invalid markdown syntax.")
+
+        for i, _ in enumerate(split_node):
+            if i % 2 == 0:
+                if split_node[i] == '':
+                    continue
+                new_nodes.append(TextNode(split_node[i], node.text_type))
+            elif i % 2 == 1:
+                new_nodes.append(TextNode(split_node[i], text_type))
+
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    if len(old_nodes) == 0:
+        raise ValueError("The node list should not be empty.")
+
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+
+        if not images:
+            new_nodes.append(node)
+            continue
+        
+        texts = node.text
+        for i, image in enumerate(images):
+            texts = texts.split(f"![{image[0]}]({image[1]})", maxsplit=1)
+            if not texts[0] == '':
+                new_nodes.append(TextNode(texts[0], "text"))
+            new_nodes.append(TextNode(image[0], "image", image[1]))
+            if len(images) == i+1 and not texts[1] == '':  # we are at the last image. there might be some text after the last image, that we have to add manually.
+                new_nodes.append(TextNode(texts[1], "text"))
+            texts = texts[1]
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    if len(old_nodes) == 0:
+        raise ValueError("The node list should not be empty.")
+
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+
+        if not links:
+            new_nodes.append(node)
+            continue
+        
+        texts = node.text
+        for i, link in enumerate(links):
+            texts = texts.split(f"[{link[0]}]({link[1]})", maxsplit=1)
+            if not texts[0] == '':
+                new_nodes.append(TextNode(texts[0], "text"))
+            new_nodes.append(TextNode(link[0], "link", link[1]))
+            if len(links) == i+1 and not texts[1] == '':  # we are at the last link. there might be some text after the last link, that we have to add manually.
+                new_nodes.append(TextNode(texts[1], "text"))
+            texts = texts[1]
+
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    # matches = re.findall(r"!\[([^\[]*)\]\(([^\(]*)\)", text)
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+def text_to_textnodes(text):
+    new_nodes = split_nodes_delimiter([TextNode(text, "text")], "**", "bold")
+    new_nodes = split_nodes_delimiter(new_nodes, "*", "italic")
+    new_nodes = split_nodes_delimiter(new_nodes, "`", "code")
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
+    return new_nodes
+
+def markdown_to_blocks(md_doc):
+    split_result = md_doc.split("\n\n")
+    stripped_blocks = [block.strip() for block in split_result if not block.strip() == '']
+    return stripped_blocks
+
--- src/test_conversion.py
+++ src/test_conversion.py
@@ -37,6 +37,259 @@ class ConversionTest(unittest.TestCase):
         except ValueError:
             pass
 
+    def test_split_nodes(self):
+        node = TextNode("This is a **bold** text.", "text")
+        new_nodes = conversion.split_nodes_delimiter([node], "**", "bold")
+        grnd_trth = [TextNode("This is a ", "text"), TextNode("bold", "bold"), TextNode(" text.", "text")]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode("**bold** text.", "text")
+        new_nodes = conversion.split_nodes_delimiter([node], "**", "bold")
+        grnd_trth = [TextNode("bold", "bold"), TextNode(" text.", "text")]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode("**bold**", "text")
+        new_nodes = conversion.split_nodes_delimiter([node], "**", "bold")
+        grnd_trth = [TextNode("bold", "bold")]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode("This is a *italic* text.", "text")
+        new_nodes = conversion.split_nodes_delimiter([node], "*", "italic")
+        grnd_trth = [TextNode("This is a ", "text"), TextNode("italic", "italic"), TextNode(" text.", "text")]
+        self.assertEqual(new_nodes, grnd_trth)
+        
+        node = TextNode("This is a `code block` text.", "text")
+        new_nodes = conversion.split_nodes_delimiter([node], "`", "code")
+        grnd_trth = [TextNode("This is a ", "text"), TextNode("code block", "code"), TextNode(" text.", "text")]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode("This is a normal text.", "text")
+        new_nodes = conversion.split_nodes_delimiter([node], "*", "italic")
+ 
+        old_nodes = [
+                    TextNode("This is ", "text"),
+                    TextNode("italic", "italic"),
+                    TextNode(" and this is **bold** text.", "text"),
+                ]
+        new_nodes = conversion.split_nodes_delimiter(old_nodes, "**", "bold")
+        grnd_trth = [
+                    TextNode("This is ", "text"),
+                    TextNode("italic", "italic"),
+                    TextNode(" and this is ", "text"),
+                    TextNode("bold", "bold"),
+                    TextNode(" text.", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        try:
+            node = TextNode("This is a normal **broken text.", "text")
+            new_nodes = conversion.split_nodes_delimiter([node], "**", "bold")
+            assert False, "A Syntax Error should have been thrown, but wasn't."
+        except SyntaxError:
+            pass
+
+        try:
+            node = TextNode("This is a **really** **broken text.", "text")
+            new_nodes = conversion.split_nodes_delimiter([node], "**", "bold")
+            assert False, "A Syntax Error should have been thrown, but wasn't."
+        except SyntaxError:
+            pass
+        
+        try:
+            new_nodes = conversion.split_nodes_delimiter([], "**", "code")
+            assert False, "A Value Error should have been thrown, but wasn't."
+        except ValueError:
+            pass
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) some more text",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_image([node])
+        grnd_trth = [
+                    TextNode("This is text with a ", "text"),
+                    TextNode("rick roll", "image", "https://i.imgur.com/aKaOqIh.gif"),
+                    TextNode(" and ", "text"),
+                    TextNode("obi wan", "image", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                    TextNode(" some more text", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "![rick roll](https://i.imgur.com/aKaOqIh.gif)![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_image([node])
+        grnd_trth = [
+                    TextNode("rick roll", "image", "https://i.imgur.com/aKaOqIh.gif"),
+                    TextNode("obi wan", "image", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "![rick roll](https://i.imgur.com/aKaOqIh.gif)![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) some more text",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_image([node])
+        grnd_trth = [
+                    TextNode("rick roll", "image", "https://i.imgur.com/aKaOqIh.gif"),
+                    TextNode("obi wan", "image", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                    TextNode(" some more text", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_image([node])
+        grnd_trth = [
+                    TextNode("", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "some text",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_image([node])
+        grnd_trth = [
+                    TextNode("some text", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg) some more text",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_link([node])
+        grnd_trth = [
+                    TextNode("This is text with a ", "text"),
+                    TextNode("rick roll", "link", "https://i.imgur.com/aKaOqIh.gif"),
+                    TextNode(" and ", "text"),
+                    TextNode("obi wan", "link", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                    TextNode(" some more text", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "[rick roll](https://i.imgur.com/aKaOqIh.gif)[obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_link([node])
+        grnd_trth = [
+                    TextNode("rick roll", "link", "https://i.imgur.com/aKaOqIh.gif"),
+                    TextNode("obi wan", "link", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "[rick roll](https://i.imgur.com/aKaOqIh.gif)[obi wan](https://i.imgur.com/fJRm4Vk.jpeg) some more text",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_link([node])
+        grnd_trth = [
+                    TextNode("rick roll", "link", "https://i.imgur.com/aKaOqIh.gif"),
+                    TextNode("obi wan", "link", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                    TextNode(" some more text", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_link([node])
+        grnd_trth = [
+                    TextNode("", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+        node = TextNode(
+            "some text",
+            "text",
+        )
+        new_nodes = conversion.split_nodes_link([node])
+        grnd_trth = [
+                    TextNode("some text", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+
+
+    def test_img_regex(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = conversion.extract_markdown_images(text)
+        grnd_trth = [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')]
+        self.assertEqual(matches, grnd_trth)
+
+        text = "This is text with a ![](https://i.imgur.com/aKaOqIh.gif) and ![](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = conversion.extract_markdown_images(text)
+        grnd_trth = [('', 'https://i.imgur.com/aKaOqIh.gif'), ('', 'https://i.imgur.com/fJRm4Vk.jpeg')]
+        self.assertEqual(matches, grnd_trth)
+
+    def test_link_regex(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = conversion.extract_markdown_links(text)
+        grnd_trth = [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')]
+        self.assertEqual(matches, grnd_trth)
+
+        text = "This is text with a link [](https://www.boot.dev) and [](https://www.youtube.com/@bootdotdev)"
+        matches = conversion.extract_markdown_links(text)
+        grnd_trth = [('', 'https://www.boot.dev'), ('', 'https://www.youtube.com/@bootdotdev')]
+        self.assertEqual(matches, grnd_trth)
+
+
+    def test_text_to_textnodes(self):
+        text = "This is a text with some **bold** and *italic* words, as well as some `code segment`. Have some ![images](https://google.com/image.jpg) and [links](https://google.com/maps) too!"
+        new_nodes = conversion.text_to_textnodes(text)
+        grnd_trth = [
+                    TextNode("This is a text with some ", "text"),
+                    TextNode("bold", "bold"),
+                    TextNode(" and ", "text"),
+                    TextNode("italic", "italic"),
+                    TextNode(" words, as well as some ", "text"),
+                    TextNode("code segment", "code"),
+                    TextNode(". Have some ", "text"),
+                    TextNode("images", "image", "https://google.com/image.jpg"),
+                    TextNode(" and ", "text"),
+                    TextNode("links", "link", "https://google.com/maps"),
+                    TextNode(" too!", "text"),
+                ]
+        self.assertEqual(new_nodes, grnd_trth)
+
+    def test_md_to_blocks(self):
+        md_doc = '''
+
+# This is a heading
+
+This is a normal text with **bold elements** and 'code blocks'.
+
+* Here we have a block
+* of bold text
+* as you can see
+
+'''
+        blocks = conversion.markdown_to_blocks(md_doc)
+        grnd_trth = [
+                    "# This is a heading",
+                    "This is a normal text with **bold elements** and 'code blocks'.",
+'''* Here we have a block
+* of bold text
+* as you can see''',
+                ]
+        self.assertEqual(blocks, grnd_trth)
+
+        md_doc = ""
+        blocks = conversion.markdown_to_blocks(md_doc)
+        grnd_trth = []
+        self.assertEqual(blocks, grnd_trth)
+                        
+
+
 if __name__ == "__main__":
     unittest.main()
 
